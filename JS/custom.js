c3dba78434e84df08280b2d68c28dc25333b972e
MkLHX <mickael.lehoux@gmail.com>
2016-09-09 13:44:42 +0200

    html repare

--- JS/custom.js
+++ JS/custom.js
@@ -1,52 +1,13 @@
 $(function(){
-<<<<<<< HEAD
-	$('nav').affix({
-      offset: {
-        top: $('top').height()
-      }
-});	
+		$('nav').affix({
+	      offset: {
+	        top: $('top').height()
+	      }
+	});	
 
-$('#content').affix({
-      offset: {
-        top: 200
-      }
-});	
+	$('#content').affix({
+	      offset: {
+	        top: 200
+	      }
+	});	
 });
-
-
-
-
-
-
-
-=======
-	/*$(window).scroll(function(){
-		scrollPosition = $(window).scrollTop();
-	});
-	$("#btn0").click(function(){
-		alert("Hello btn0")});
-	$("#btn1").click(function(){
-		alert("Hello btn1")});
-	$("#btn2").click(function(){
-		alert("Hello btn2")});
-	$("#btn3").click(function(){
-		alert("Hello btn3")});
-	$("#btn4").click(function(){
-		alert("Hello btn4")});
-    $('#btn0').click(function(){    
-    $('.content').animate({
-        scrollTop: 340
-    }, 1000);
-	});
-	$('#btn1').click(function(){    
-    $('.content').animate({
-        scrollTop: 340
-    }, 1000);
-	});
-	$('#btn2').click(function(){    
-		$('.content').animate({
-			scrollTop: 0
-		}, 1000);
-	});*/
-});
->>>>>>> f2de70ebf208c0c1556632b4619cb1c20df149b7
