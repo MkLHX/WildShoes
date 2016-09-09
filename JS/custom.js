3f4f71422cc56cf23e0a86da500fc12d833df28e
Guillaume4545 <guillaumecuriel@gmail.com>
2016-09-09 11:33:36 +0200

    add modif

--- JS/custom.js
+++ JS/custom.js
@@ -1,30 +1,20 @@
 $(function(){
-	/*$(window).scroll(function(){
-		scrollPosition = $(window).scrollTop();
-	});
-	$("#btn0").click(function(){
-		alert("Hello btn0")});
-	$("#btn1").click(function(){
-		alert("Hello btn1")});
-	$("#btn2").click(function(){
-		alert("Hello btn2")});
-	$("#btn3").click(function(){
-		alert("Hello btn3")});
-	$("#btn4").click(function(){
-		alert("Hello btn4")});
-    $('#btn0').click(function(){    
-    $('.content').animate({
-        scrollTop: 340
-    }, 1000);
-	});
-	$('#btn1').click(function(){    
-    $('.content').animate({
-        scrollTop: 340
-    }, 1000);
-	});
-	$('#btn2').click(function(){    
-		$('.content').animate({
-			scrollTop: 0
-		}, 1000);
-	});*/
+	$('nav').affix({
+      offset: {
+        top: $('top').height()
+      }
+});	
+
+$('#content').affix({
+      offset: {
+        top: 200
+      }
+});	
 });
+
+
+
+
+
+
+
