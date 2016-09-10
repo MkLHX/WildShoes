128d1550a0bf3ef9927a48ec71be268cdde49f45
MkLHX <mickael.lehoux@gmail.com>
2016-09-10 10:16:43 +0200

    ajout javascript pour animation navigation

--- JS/custom.js
+++ JS/custom.js
@@ -1,19 +1,33 @@
 $(function(){
-	/*$('nav').affix({
-	      offset: {
-	        top: $('top').height()
-	      }
-	});	
-
-	$('#content').affix({
-	      offset: {
-	        top: 200
-	      }
-	});*/
-	/*Animation scrolling vers une section*/
-	/*$("button").click(function() {
+	/*##### Animation scrolling vers une section ####*/
+	/*Navigation vers rubrique historique*/
+	$("#bphistorique").click(function() {
 	    $('html,body').animate({
-	        scrollTop: $(".second").offset().top},
+	        scrollTop: $("#historique").offset().top},
 	        'slow');
-	});*/
+	});
+	/*Navigation vers rubrique chaussespieds*/
+	$("#bpchaussespieds").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $("#chaussespieds").offset().top},
+	        'slow');
+	});
+	/*Navigation vers rubrique tiresbottes*/
+	$("#bptiresbottes").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $("#tiresbottes").offset().top},
+	        'slow');
+	});
+	/*Navigation vers rubrique personalise*/
+	$("#bppersonalise").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $("#personalise").offset().top},
+	        'slow');
+	});
+	/*Navigation vers rubrique footer*/
+	$("#bpfooter").click(function() {
+	    $('html,body').animate({
+	        scrollTop: $("#footer").offset().top},
+	        'slow');
+	});
 });
