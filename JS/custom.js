aaab3f9b561994b4989ae016de1bf6b1cb91ed8f
MkLHX <mickael.lehoux@gmail.com>
2016-09-13 16:52:26 +0200

    modification js pour fonction mailto

--- JS/custom.js
+++ JS/custom.js
@@ -1,46 +1,50 @@
 $(function(){
 	/*##### Animation scrolling vers une section ####*/
 	/*Navigation to accueil*/
-	$("#bpaccueil").click(function() {
+	$('#bpaccueil').click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("header").offset().top},
+	        scrollTop: $('header').offset().top},
 	        'slow');
 	});
 	/*Navigation to historique*/
-	$("#bphistorique").click(function() {
+	$('#bphistorique').click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#historique").offset().top},
+	        scrollTop: $('#historique').offset().top},
 	        'slow');
 	});
 	/*Navigation to chaussespieds*/
-	$("#bpchaussespieds").click(function() {
+	$('#bpchaussespieds').click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#chaussespieds").offset().top},
+	        scrollTop: $('#chaussespieds').offset().top},
 	        'slow');
 	});
 	/*Navigation to tiresbottes*/
-	$("#bptiresbottes").click(function() {
+	$('#bptiresbottes').click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#tiresbottes").offset().top},
+	        scrollTop: $('#tiresbottes').offset().top},
 	        'slow');
 	});
 	/*Navigation to personalise*/
-	$("#bppersonalise").click(function() {
+	$('#bppersonalise').click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#personalise").offset().top},
+	        scrollTop: $('#personalise').offset().top},
 	        'slow');
 	});
 	/*Navigation to footer*/
-	$("#bpfooter").click(function() {
+	$('#bpfooter').click(function() {
 	    $('html,body').animate({
-	        scrollTop: $("#footer").offset().top},
+	        scrollTop: $('#footer').offset().top},
 	        'slow');
 	});
 	/*##### Mailto ####*/
 	/*Using contact form to create mailto content*/
 	$('#submitEmail').click(function(){
+		/*Use the textarea content to put in body mailto*/
 		var body = $('#emailContent').val();
+		/*Use the email form to send to contact for response*/
 		var email = $('#emailReponse').val();
-		$(this).attr("href", "mailto:contact@wildshoes.com?body="+body+"%0D%0AMerci de me répondre à l'adresse suivante : "+email);
+		/*Add href link with mailto content and close modal contact*/
+		$(this).attr('href', 'mailto:contact@wildshoes.com?body='+body+'%0D%0AMerci de me répondre à l\'adresse suivante : '+email);
+		$('#contact').modal('toggle');
 	})
 });
