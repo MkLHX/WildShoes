686fa147dc3c993309e7d0d9bd2eb66fe5a38544
MkLHX <mickael.lehoux@gmail.com>
2016-09-13 16:32:05 +0200

    modification des articles et ajout fonctionnalité envoi email

--- JS/custom.js
+++ JS/custom.js
@@ -1,39 +1,46 @@
 $(function(){
 	/*##### Animation scrolling vers une section ####*/
-	/*Navigation vers rubrique accueil*/
+	/*Navigation to accueil*/
 	$("#bpaccueil").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $("header").offset().top},
 	        'slow');
 	});
-	/*Navigation vers rubrique historique*/
+	/*Navigation to historique*/
 	$("#bphistorique").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $("#historique").offset().top},
 	        'slow');
 	});
-	/*Navigation vers rubrique chaussespieds*/
+	/*Navigation to chaussespieds*/
 	$("#bpchaussespieds").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $("#chaussespieds").offset().top},
 	        'slow');
 	});
-	/*Navigation vers rubrique tiresbottes*/
+	/*Navigation to tiresbottes*/
 	$("#bptiresbottes").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $("#tiresbottes").offset().top},
 	        'slow');
 	});
-	/*Navigation vers rubrique personalise*/
+	/*Navigation to personalise*/
 	$("#bppersonalise").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $("#personalise").offset().top},
 	        'slow');
 	});
-	/*Navigation vers rubrique footer*/
+	/*Navigation to footer*/
 	$("#bpfooter").click(function() {
 	    $('html,body').animate({
 	        scrollTop: $("#footer").offset().top},
 	        'slow');
 	});
+	/*##### Mailto ####*/
+	/*Using contact form to create mailto content*/
+	$('#submitEmail').click(function(){
+		var body = $('#emailContent').val();
+		var email = $('#emailReponse').val();
+		$(this).attr("href", "mailto:contact@wildshoes.com?body="+body+"%0D%0AMerci de me répondre à l'adresse suivante : "+email);
+	})
 });
