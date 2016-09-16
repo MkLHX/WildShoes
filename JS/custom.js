c4995975540a75d2f12f800e5213c953e9ce1fe6
MkLHX <mickael.lehoux@gmail.com>
2016-09-16 11:14:39 +0200

    modif js for styling navbar during scroll on large and small screens

--- JS/custom.js
+++ JS/custom.js
@@ -46,39 +46,38 @@ $(function(){
 		/*Add href link with mailto content and close modal contact*/
 		$(this).attr('href', 'mailto:contact@wildshoes.com?body='+body+'%0D%0AMerci de me répondre à l\'adresse suivante : '+email);
 		$('#contact').modal('toggle');
-	})
-	$('.easterEgg').mouseover(function(){
-		$(this).show();
 	});
-	if ($(window).width() < 1300) {
-	   var scrollStart = $(window).scrollTop();
-		if (scrollStart > 400) {
+
+	/*##### Navbar Color change ####*/
+	/*For screen size under 1000px width remove class navbarClear on load and scroll screen*/
+	/*Statements on load*/
+	if ($(window).width() < 769) {
 			$("nav").removeClass("navbarClear");
-		}
-		$(window).scroll(function() {
-			var scroll = $(window).scrollTop();
-		    if (scroll > 400) {
-		        $("nav").removeClass("navbarClear");
-		    } 
-		    else {
-		        $("nav").addClass("navbarClear");
-		    }
-		});
-	}
-	else {
+	} else {
 		var scrollStart = $(window).scrollTop();
-		if (scrollStart > 910) {
+		if (scrollStart > 500) {
 			$("nav").removeClass("navbarClear");
+		} else if (scrollStart > 910){
+			$("nav").removeClass("navbarClear");
+		} else {
+			$("nav").addClass("navbarClear");
 		}
-		$(window).scroll(function() {
-			var scroll = $(window).scrollTop();
-		    if (scroll > 910) {
-		        $("nav").removeClass("navbarClear");
-		    } 
-		    else {
-		        $("nav").addClass("navbarClear");
-		    }
-		});
 	}
-	
+	/*Statements for scroll*/
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		if ($(window).width() < 769) {
+			$("nav").removeClass("navbarClear");
+		} else if ($(window).width() < 1300) {
+		    if (scroll > 500) {
+		    	$("nav").removeClass("navbarClear");
+		    } else {
+		    	$("nav").addClass("navbarClear");
+		    }
+		} else if (scroll > 910) {
+			$("nav").removeClass("navbarClear");
+		} else {
+			$("nav").addClass("navbarClear");
+		}
+	});
 });
